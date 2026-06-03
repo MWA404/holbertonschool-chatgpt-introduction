6c5081c9077a1b7683dd0d6bd67d9e9af5372a38
MWA404 <mmmo20148@gmail.com>
2026-06-03 14:07:00 +0300

    documentation for factorial_recursive.py

---
+++ debugging/factorial_recursive.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+import sys
+
+def factorial(n):
+     """
+    Computes the factorial of a given number recursively.
+    For example, factorial(5) returns 120 because 5 * 4 * 3 * 2 * 1 = 120.
+
+    Parameters:
+        n (int): The number to calculate the factorial for. Must be 0 or positive.
+
+    Returns:
+        int: The factorial of n. If n is 0, returns 1 (since 0! = 1).
+    """
+    if n == 0:
+        return 1
+    else:
+        return n * factorial(n-1)
+
+f = factorial(int(sys.argv[1]))
+print(f)
